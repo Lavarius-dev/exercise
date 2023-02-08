383ec0c16355249f9f2699a14a0f81f2d25f8c0e
Lavarius <okhrimenko.yura99@gmail.com>
2023-02-09 00:25:28 +0500

    Fixed files

--- src/main/java/dev/lavarius/exercise/controller/DocumentController.java
+++ src/main/java/dev/lavarius/exercise/controller/DocumentController.java
@@ -3,22 +3,13 @@ package dev.lavarius.exercise.controller;
 import dev.lavarius.exercise.model.Document;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 @RequestMapping("/documents")
 public class DocumentController {
-
-    private List<Document> documents = List.of(
-            new Document(1L, "subj_1", "auth_1",
-                    "perf_1, perf_2", "2 years",
-                    false, false, "information 1"),
-            new Document(2L, "subj_2", "auth_2",
-                    "perf_3, perf_4", "5 years",
-                    false, false, "information 2"),
-            new Document(3L, "subj_3", "auth_3",
-                    "perf_5, perf_6", "13 years",
-                    false, false, "information 3")
-    );
+    private List<Document> documents = new ArrayList<>();
 
     @GetMapping()
     public List<Document> getAllDocuments() {
@@ -26,23 +17,22 @@ public class DocumentController {
     }
 
     @GetMapping("/{id}")
-    public Document getDocumentById(@PathVariable Long id) {
+    public Document getDocumentById(@PathVariable Integer id) {
         return documents.stream().filter(document -> document.getId().equals(id)).findAny().orElse(null);
     }
 
     @PostMapping("/create")
-    public String createDocument(Document document) {
+    public Document createDocument(@RequestBody Document document) {
         documents.add(document);
-        return "redirect:/documents";
+        return document;
     }
 
     @DeleteMapping("/delete/{id}")
-    public String deleteDocument(@PathVariable Long id) {
+    public void deleteDocument(@PathVariable Integer id) {
         documents.removeIf(document -> document.getId().equals(id));
-        return "redirect:/documents";
     }
 
-    public String editDocument() {
+    public Document editDocument() {
         return null;
     }
 
